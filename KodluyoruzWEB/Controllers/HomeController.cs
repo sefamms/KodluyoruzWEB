c394d40abfb722ce1b641ca19a554858b5edfc4d
sefamms <smemis412@gmail.com>
2020-12-26 17:11:43 +0300

    Add project files.

---
+++ KodluyoruzWEB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using KodluyoruzWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KodluyoruzWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var current = Directory.GetCurrentDirectory();
+            ViewBag.current = current;
+            return View("~/Views/Product/Index.cshtml");
+        }
+
+      
+
+    }
+}
---
+++ KodluyoruzWEB/Models/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KodluyoruzWEB.Models
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public  Product product { get; set; }
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+    
+    }
+}
